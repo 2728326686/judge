d7fddb2e34dc2f159c7a4817581d31001ed373cc
mango <2728326686@qq.com>
2024-12-22 02:29:38 +0800

    创建项目

---
+++ src/test/java/com/hyj/judge/JudgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyj.judge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JudgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
